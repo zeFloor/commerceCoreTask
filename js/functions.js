271dc47572a535d7fd7b7bc32bc2aa9b63bf4f11
zeFloor <erviss1@gmail.com>
2022-01-25 23:06:55 +0200

    finished with javaScript functions

--- js/functions.js
+++ js/functions.js
@@ -19,49 +19,89 @@ orderSummary.onclick = function(e) {
         arrow.classList.remove("fa-chevron-up")
         arrow.classList.add("fa-chevron-right")
     }
-    e.stopPropagation()
 }
 
-// Put on the event listener so that the shopping list would disappear when clicked anywhere in DOM except for the shopping list
-document.addEventListener('click', e => {
-    if(e.target.closest(".shoppingList") === null) {
-        shoppingList.removeAttribute("id")
-        shoppingList.id = "disappear"
-        arrow.classList.remove("fa-chevron-up")
-        arrow.classList.add("fa-chevron-right")
-    }
-})
+// -------------------- END OF FUNCTION --------------------
 
-// ---------- END OF FUNCTION ----------
-
-// ---------- Function that fills up shopping list with items and removes them ----------
+// ---------- Functions that fills up shopping list with items and removes them ----------
 
 var shoppingCart = document.querySelector(".shoppingItems")
 var removeItem = document.querySelector(".remove")
-var product = document.querySelector('.shoppingItem')
+var totalPrice = document.querySelectorAll(".price")
 
-shoppingItems = [['threeLeggings', '3x CoreProduct', 39.99], ['leggings', 'Extra CoreProduct', 9.99]]
+//Function factory to create object from shopping items
+
+const shoppingItems= (itemName, itemPrice) => {
+    if(itemName === 'leggings'){
+        product = "1x Extra CoreProduct"
+    } else if(itemName === 'threeLeggings'){
+        product = "3x CoreProduct"
+    }
+    return{itemName, itemPrice, product}
+}
+
+//Object instantiations
+
+const leggings = shoppingItems('leggings', 9.99)
+const threeLeggings = shoppingItems('threeLeggings', 39.99)
+
+//Filling up a list with Objects
+
+shoppingArray = [threeLeggings, leggings]
+
+//Function that takes data from Objects and inserts them in the DOM
 
 function addShoppingItem() {
-    shoppingItems.forEach(element => {
+    shoppingArray.forEach(element => {
         shoppingCart.innerHTML += `
         <li class="flex-container shoppingItem">
             <div class="flex-container imgAndProdName">
-            <img src="images/${element[0]}.svg" alt="">
+                <img src="images/${element.itemName}.svg" alt="">
                 <div class="product">
-                    <p>${element[1]}&#174</p>
+                    <p>${element.product}&#174</p>
                     <button class="remove" onclick="removeShoppingItem(this)">Remove</button>
                 </div>
             </div>
-            <p>$${element[2]}</p>
+            <p class="itemPrice">$${element.itemPrice}</p>
         </li>
         
-        `
-    });
+        `    
+    })
 }
+
+//Function that calculates the total price of shopping cart
+
+function calculateTotalPrice() {
+    let price = 0
+    shoppingArray.forEach(element => {
+        price += element.itemPrice
+    });
+    totalPrice.forEach(element => {
+        element.innerHTML = `$${parseFloat(price).toFixed(2)}`
+    })
+} 
+
+//Function that indexes shopping items inside shopping cart
+
+function indexShoppingItems() {
+    for(let i = 0; i < shoppingCart.children.length; i++) {
+    shoppingCart.children[i].setAttribute('data-index', i)
+    }
+}
+
+//Function that removes shopping item from shopping cart and resets the cart
 
 function removeShoppingItem(e) {
-    e.parentNode.parentNode.parentNode.remove()
+    shoppingArray.splice(e.closest(".shoppingItem").dataset.index,1)
+    e.closest(".shoppingItem").remove()
+    shoppingCart.innerHTML = ''
+    addShoppingItem()
+    calculateTotalPrice()
+    indexShoppingItems()
 }
 
+// -------------------- END OF SHOPPING LIST FUNCTIONS --------------------
+
 addShoppingItem()
+calculateTotalPrice()
+indexShoppingItems()
